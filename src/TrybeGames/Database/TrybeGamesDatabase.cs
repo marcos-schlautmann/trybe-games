59091d8c399d734d96b43cf719a70ad857e74a3a
Marcos <marcos27.bonfim@gmail.com>
2023-10-06 18:39:57 -0300

    req07 e req08 ok

--- src/TrybeGames/Database/TrybeGamesDatabase.cs
+++ src/TrybeGames/Database/TrybeGamesDatabase.cs
@@ -68,8 +68,10 @@ public class TrybeGamesDatabase
     // 8. Crie a funcionalidade de buscar todos os diferentes Tipos de jogos dentre os jogos cadastrados
     public List<GameType> GetGameTypes()
     {
-        // Implementar
-        throw new NotImplementedException();
+        var gameTypes = from game in Games
+                        select game.GameType;
+
+        return gameTypes.Distinct().ToList();
     }
 
     // 9. Crie a funcionalidade de buscar todos os estúdios de jogos junto dos seus jogos desenvolvidos com suas pessoas jogadoras
